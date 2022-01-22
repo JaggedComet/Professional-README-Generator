47cfc443570f126e0b8f2216299ac32f4255522c
Marco Flores <marcoflores1029@gmail.com>
2022-01-22 15:45:20 -0800

    finished index.js with questions linked to template

--- index.js
+++ index.js
@@ -2,83 +2,110 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const template = require("./readmetemplate");
 
-inquirer
-    .prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'location',
-            message: 'Where do you live?',
-        },
-        {
-            type: 'input',
-            name: 'hobby',
-            message: 'What is your hobby?',
-        },
-        {
-            type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
-        },
-        {
-            type: "list",
-            message: "Choose a license to create your badge:",
-            name: "license",
-            choices: [{
-                    name: "MIT license",
-                    value: "MIT"
-                },
-                {
-                    name: "Apache License 2.0",
-                    value: "Apache"
-                },
-                {
-                    name: 'BSD 3-Clause "New" or "Revised" license',
-                    value: "BSD3"
-                },
-                {
-                    name: "GNU General Public License (GPL)",
-                    value: "GPL"
-                },
-            ],
-        },
-    ])
-    .then(({
-        license
-    }) => {
-        // Create a license string
-        let badge;
-        switch (license) {
-            case "MIT":
-                badge =
-                    "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-                break;
-            case "Apache":
-                badge =
-                    "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-                break;
-            case "BSD3":
-                badge =
-                    "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-                break;
-            case "GPL":
-                badge =
-                    "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-                break;
+const init = () => {
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Describe the functionality of your product (motivation, problem it solved, what did you learn).',
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'What are the instructions and examples for use? Include screenshots as needed.',
+            },
+            {
+                type: 'input',
+                name: 'contribution',
+                message: 'List your collaborators if you had any.',
+            },
+            {
+                type: 'input',
+                name: 'features',
+                message: 'What are the features in your project?',
+            },
+            {
+                type: 'input',
+                name: 'third',
+                message: 'If you would like anyone to contribute, please put how they can.',
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'If you have any tests for your application, put them here.',
+            },
+            {
+                type: "list",
+                message: "Choose a license to create your badge:",
+                name: "license",
+                choices: [{
+                        name: "MIT license",
+                        value: "MIT"
+                    },
+                    {
+                        name: "Apache License 2.0",
+                        value: "Apache"
+                    },
+                    {
+                        name: 'BSD 3-Clause "New" or "Revised" license',
+                        value: "BSD3"
+                    },
+                    {
+                        name: "GNU General Public License (GPL)",
+                        value: "GPL"
+                    },
+                ],
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your github link?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email that developers can reach you at?',
+            },
+        ])
+        .then(data => {
+            // Create a license string
+            let badge;
+            switch (data.license) {
+                case "MIT":
+                    badge =
+                        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+                    break;
+                case "Apache":
+                    badge =
+                        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+                    break;
+                case "BSD3":
+                    badge =
+                        "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+                    break;
+                case "GPL":
+                    badge =
+                        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+                    break;
+            }
 
-            default:
-                throw new Error(`Invalid license ${license}`);
-        }
+            const readMeDocument = template(data, badge);
 
-        const markdown = `# License Badge
+            // Save to file
+            fs.writeFile("README.md", readMeDocument, (err) =>
+                err ? console.error(err) : console.log("Created README.md")
+            );
+        });
+}
 
-${badge}`;
-
-        // Save to file
-        fs.writeFile("badge.md", markdown, (err) =>
-            err ? console.error(err) : console.log("Created badge.md")
-        );
-    });
+init ();
--- readmetemplate.js
+++ readmetemplate.js
@@ -1,3 +1,49 @@
-const template = ({});
+const template = ({ description, installation, usage, contribution, license, features, third, tests, title }, badge) =>
+
+`# ${title}
+${badge}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Credits
+
+${contribution}
+
+## License
+
+${license}
+
+---
+
+## Features
+
+${features}
+
+## How to Contribute
+
+${third}
+
+## Tests
+
+${tests}
+
+`
 
 module.exports = template;
